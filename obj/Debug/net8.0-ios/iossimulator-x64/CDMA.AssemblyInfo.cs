b9cccfb23f0b417c63c8f2a2932fbfae083e66aa
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-09-26 20:44:54 +0530

    modified:   MainPage.xaml         modified:   Platforms/Android/Resources/drawable/splash_screen.png         modified:   bin/Debug/

--- obj/Debug/net8.0-ios/iossimulator-x64/CDMA.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/CDMA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+754fd0e8cd71c368465f7712dffa5408cd389249")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+539ea6ebbd636709defb132c140aeee62e299548")]
 [assembly: System.Reflection.AssemblyProductAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
