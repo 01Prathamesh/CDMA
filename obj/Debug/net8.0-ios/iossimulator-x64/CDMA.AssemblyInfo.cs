35c3cb2a4f30933b33a6a9ae3ca50dab61c5b678
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-09-26 17:01:41 +0530

    modified:   CDMA.csproj         new file:   Resources/AppIcon/Mipmap/mipmap-hdpi/icon.png         new file:   Resources/AppIcon/Mipmap/mipmap-mdpi/icon.png         new file:   Resources/AppIcon/Mipmap/mipmap-xhdpi/icon.png         new file:   Resources/AppIcon/Mipmap/mipmap-xxhdpi/icon.png         new file:   Resources/AppIcon/Mipmap/mipmap-xxxhdpi/icon.png         deleted:    Resources/AppIcon/appicon.svg         modified:   obj/Debug/

--- obj/Debug/net8.0-ios/iossimulator-x64/CDMA.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/CDMA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d9d3dba670e43c045d24836c0180120253cdf1ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
