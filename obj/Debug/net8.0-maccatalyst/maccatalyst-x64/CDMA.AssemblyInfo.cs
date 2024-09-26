c05cf13504c1836e69a345d9fefafb5cdf4b49d8
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-09-26 20:07:22 +0530

    modified:   CDMA.csproj         modified:   bin/Debug/

--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/CDMA.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/CDMA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1dfcefc8f1b78a02a121887962d4d1f0461dc52d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+221290dd93654614ac976966d4c3ef63e7a921cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
