221290dd93654614ac976966d4c3ef63e7a921cd
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-09-26 19:55:51 +0530

    [200~ modified:   CDMA.csproj

--- obj/Debug/net8.0-ios/iossimulator-x64/CDMA.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/CDMA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6febaabe89e78dce5873fd638798f61f01f7832b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+1dfcefc8f1b78a02a121887962d4d1f0461dc52d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
