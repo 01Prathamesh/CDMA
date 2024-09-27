6429d138f26ca8589943d6accfbb8cb311d0e285
Prathamesh Kasar <prathameshkasar000@gmail.com>
2024-09-27 11:05:14 +0530

    modified:   MainPage.xaml         modified:   obj/Debug/

--- obj/Debug/net8.0-ios/iossimulator-x64/CDMA.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/CDMA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+15ea8087ad6bea395fbfc3883c945b95430fb7f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b28ce9f4ef7c8f6d93e0784762176770aac347d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CDMA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
